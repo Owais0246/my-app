8d428f24df26cfd322d720a9c524ba79513b2270
Owais0246 <owaiskazi24@gmail.com>
2024-01-09 17:29:16 +0530

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,Button} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to  your app! hello jjDj</Text>
       <StatusBar style="auto" />
+      <Button title='Press' style={{ marginTop: 20 ,backgroundColor:"red"}}/>
     </View>
   );
 }
